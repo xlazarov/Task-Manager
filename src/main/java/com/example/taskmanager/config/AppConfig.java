7865c53146f36145eec98526b6c247eeb6d14cf5
Keťa <03m.lazarova@gmail.com>
2023-12-14 18:07:33 +0100

    Bean clean-up

--- src/main/java/com/example/taskmanager/config/AppConfig.java
+++ src/main/java/com/example/taskmanager/config/AppConfig.java
@@ -4,20 +4,15 @@ import com.example.taskmanager.data.TaskRepository;
 import com.example.taskmanager.data.UserRepository;
 import com.example.taskmanager.service.TaskService;
 import com.example.taskmanager.service.UserService;
-import com.example.taskmanager.web.TaskController;
-import com.example.taskmanager.web.UserController;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableMBeanExport;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
-@EnableMBeanExport // Enable JMX
+@EnableMBeanExport
 public class AppConfig {
 
     @Bean
-    @Scope("singleton") // by default though
     public TaskService taskService(TaskRepository taskRepository) {
         return new TaskService(taskRepository);
     }
@@ -27,19 +22,4 @@ public class AppConfig {
         return new UserService(userRepository);
     }
 
-    @Bean
-    public TaskController taskController(TaskService taskService) {
-        return new TaskController(taskService);
-    }
-
-    @Bean
-    public UserController userController(UserService userService) {
-        return new UserController(userService);
-    }
-
-    @Bean
-    @ConfigurationProperties(prefix = "custom")
-    public CustomProperties customProperties() {
-        return new CustomProperties();
-    }
 }
